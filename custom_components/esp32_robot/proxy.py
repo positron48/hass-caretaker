1292c2b273fbbc93299f8d49cac9226807ac0470
Filatov Anton <filatov@intaro.ru>
2025-04-03 20:32:31 +0300

    fix auth component

--- custom_components/esp32_robot/proxy.py
+++ custom_components/esp32_robot/proxy.py
@@ -10,9 +10,8 @@ import voluptuous as vol
 from homeassistant.components.http import HomeAssistantView, KEY_HASS, KEY_AUTHENTICATED
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from homeassistant.components.http.auth import async_sign_path
+from homeassistant.components.http.auth import async_sign_path, async_get_user_from_request
 from homeassistant.components.http.ban import process_success_login
-from homeassistant.components.http.auth import async_get_user_from_request
 
 _LOGGER = logging.getLogger(__name__)
 
